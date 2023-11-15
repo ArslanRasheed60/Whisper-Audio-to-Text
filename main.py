c44d6380181f7dc07b18a9bce8013922941322d8
Arslan Rasheed <arslanrashee1@gmail.com>
2023-11-15 19:59:19 +0500

    firebase integration and bash process added

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ import whisper
 import shutil
 import os
 import requests
+from collections import defaultdict
+from threading import Thread
+from queue import Queue
+import time
 
-from model import WhisperAudioURL
+from model import WhisperAudioURL, WhisperBashAudioURL
+# firebase 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+#user queues
+user_queues = defaultdict(Queue)
 
 app = FastAPI()
+
+cred = credentials.Certificate('./whisper.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,7 +35,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+COLLECTION_USERS = "users"
+SUB_COLLECTION_AUDIO = "AudioData"
+
+@app.get("/testing")
 async def test():
     return {"whisper":"working"}
 
@@ -44,6 +61,51 @@ async def transcribe_audio_from_url(whisperAudioUrl: WhisperAudioURL):
             return JSONResponse(content=item, status_code=200)
         else:
             raise HTTPException(status_code=500, detail="Failed to download audio file from the provided URL")
+    except Exception as e:
+        item = {"status": "failed", "message": str(e)}
+        return JSONResponse(content=item, status_code=500)
+
+@app.post("/transcribe-batch/")
+async def transcribe_bashes(whisperBashIds: WhisperBashAudioURL):
+    try:
+        user_doc_id = whisperBashIds.uid
+        sub_collec_id_list = whisperBashIds.ids
+        print(user_doc_id,sub_collec_id_list)
+        for sub_ids in sub_collec_id_list:
+            print(sub_ids)
+            doc_ref = db.collection(COLLECTION_USERS).document(user_doc_id).collection(SUB_COLLECTION_AUDIO).document(sub_ids)
+            doc_dat = doc_ref.get()
+            if doc_dat.exists:
+                doc_data = doc_dat.to_dict()
+                created_date_time = doc_data.get("createdDateTime")
+                audio_url = doc_data.get("audio")
+                model_type = doc_data.get("modelType")
+                text = doc_data.get("text")
+                print(f"Details:{model_type}, {text}, ")
+                #Download the audio file from the provided URL
+                response = requests.get(audio_url, stream=True)
+                if response.status_code == 200:
+                    with open("audio.mp3", "wb") as audio_file:
+                        shutil.copyfileobj(response.raw, audio_file)
+                    del response
+
+                    # Load the whisper model and transcribe the audio
+                    model = whisper.load_model(model_type)
+                    result = model.transcribe("audio.mp3")
+                    
+                    doc_ref = db.collection(COLLECTION_USERS) \
+                        .document(user_doc_id) 
+                    doc_ref2 = doc_ref.collection(SUB_COLLECTION_AUDIO) \
+                        .document(sub_ids) \
+                        .update({"text": result["text"]})
+                else:
+                    doc_ref = db.collection(COLLECTION_USERS) \
+                        .document(user_doc_id) 
+                    doc_ref2 = doc_ref.collection(SUB_COLLECTION_AUDIO) \
+                        .document(sub_ids) \
+                        .update({"text": "Processing Failed"})
+        
+        return JSONResponse(content={"status": "Success"}, status_code=200)
     except Exception as e:
         item = {"status": "failed", "message": str(e)}
         return JSONResponse(content=item, status_code=500)
--- model.py
+++ model.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
+from typing import List
 
 # Create a Pydantic model to define the request body
 class WhisperAudioURL(BaseModel):
     url: str
+    
+class WhisperBashAudioURL(BaseModel):
+    uid: str
+    ids: List[str]
